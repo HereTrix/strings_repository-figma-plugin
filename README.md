5267eb459ce800e5fe91731ba4555368e09b1de7
Igor B <>
2023-10-31 15:17:10 +0200

    Plugin source code

---
+++ README.md
@@ -0,0 +1,20 @@
+StringsRepository Figma plugin
+========
+
+Simple plugin for figma which uses [string repository project](https://github.com/HereTrix/strings_repository). Plugin uses only current selection to simplify work with translation.
+
+Overview
+--------
+
+Provides following features:
+
+* Connect to your own instance of [StringsRepository](https://github.com/HereTrix/strings_repository)
+* Use list of localizations added to your project
+* Pull translations from server and apply them as separate actions
+* Push existing translations to server
+* Store credentials between sessions
+
+License
+-------
+
+**StringsRepository Figma plugin** is released under the MIT license. See `LICENSE` for details.
---
+++ code.ts
@@ -0,0 +1,294 @@
+// This file holds the main code for plugins. Code in this file has access to
+// the *figma document* via the figma global object.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+
+enum MessageType {
+  setup = 'setup',
+  items = 'items',
+  languages = 'languages',
+}
+
+interface TranslationMessage {
+  token: string
+  translation: string
+}
+
+interface Credentials {
+  host: string
+  port: string
+  token: string
+}
+
+// Skip over invisible nodes and their descendants inside instances
+// for faster performance.
+figma.skipInvisibleInstanceChildren = true
+
+var credentials: Credentials | undefined = undefined
+
+var nodes: { [id: string]: string } = {}
+var languageCode = 'en'
+
+// Runs this code if the plugin is run in Figma
+if (figma.editorType === 'figma') {
+  // This plugin will open a window to prompt the user to enter a number, and
+  // it will then create that many rectangles on the screen.
+
+  // This shows the HTML page in "ui.html".
+  figma.showUI(__html__, { width: 600, height: 320 });
+
+  load()
+
+  // Calls to "parent.postMessage" from within the HTML page will trigger this
+  // callback. The callback will be passed the "pluginMessage" property of the
+  // posted message.
+  figma.ui.onmessage = msg => {
+    // One way of distinguishing between different types of messages sent from
+    // your HTML page is to use an object with a "type" property like this.
+    switch (msg.type) {
+      case 'connect':
+        if (msg.message.host && msg.message.token) {
+          var port = '8000'
+          if (msg.message.port) {
+            port = msg.message.port
+          }
+          if (credentials) {
+            credentials.host = msg.message.host
+            credentials.token = msg.message.token
+            credentials.port = port
+          } else {
+            credentials = {
+              host: msg.message.host,
+              token: msg.message.token,
+              port: port
+            }
+          }
+          storeCreds()
+
+          fetchTextNodesFromSelection()
+        } else {
+          sendError("Provide host and access token")
+        }
+        break
+      case 'refresh':
+        fetchTextNodesFromSelection()
+        break
+      case 'push':
+        push(msg.selected)
+        break
+      case 'pull':
+        pull(msg.selected)
+        break
+      case 'apply':
+        applyChanges()
+        break
+      case 'cancel':
+        figma.closePlugin();
+        break
+      case 'language':
+        languageCode = msg.value
+        break;
+      case 'logout':
+        logout()
+        break
+    }
+  };
+}
+
+function send(type: MessageType, data: any) {
+  figma.ui.postMessage({ type: type, content: data })
+}
+
+function sendError(data: any) {
+  console.log(`error: ${data}`)
+  figma.ui.postMessage({ type: 'error', content: data })
+}
+
+function fetchTextNodesFromSelection() {
+  var textNodes: { [id: string]: string } = {}
+
+  for (const node of figma.currentPage.selection) {
+    const nodes = getTextNodesFrom(node, [])
+    for (const elm of nodes) {
+      if (elm.type === "TEXT") {
+        textNodes[elm.name] = elm.characters
+      }
+    }
+  }
+
+  nodes = textNodes
+  send(MessageType.items, textNodes)
+}
+
+function getTextNodesFrom(node: SceneNode, textNodes: SceneNode[]): SceneNode[] {
+  if ('children' in node) {
+    for (const child of node.children) {
+      const subnodes = getTextNodesFrom(child, textNodes)
+      textNodes.concat(subnodes)
+    }
+  }
+
+  if (node.type === "TEXT") {
+    textNodes.push(node)
+  }
+  return textNodes
+}
+
+function updateNodes(data: TranslationMessage[]) {
+  console.log(JSON.stringify(data))
+  for (const item of data) {
+    nodes[item.token] = item.translation
+  }
+  send(MessageType.items, nodes)
+}
+
+function applyChanges() {
+  for (const selection of figma.currentPage.selection) {
+    for (const item in nodes) {
+      updateTextChild(selection, item, nodes[item])
+    }
+  }
+}
+
+async function updateTextChild(node: SceneNode, name: string, value: string) {
+  if (node.type === 'TEXT' && node.name === name) {
+    const missingFonts = node.getRangeAllFontNames(0, node.characters.length);
+    for (const font of missingFonts) {
+      await figma.loadFontAsync(font)
+    }
+    node.autoRename = false
+    node.characters = value
+  }
+  if ('children' in node) {
+    for (const child of node.children) {
+      updateTextChild(child, name, value)
+    }
+  }
+}
+
+// Network
+async function pull(items: string[]) {
+  if (!credentials) {
+    return
+  }
+
+  const data = {
+    code: languageCode,
+    tokens: items
+  }
+
+  console.log(JSON.stringify(data))
+
+  fetch(`${credentials.host}:${credentials.port}/api/plugin/pull`, {
+    method: "POST",
+    headers: headers(),
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return Promise.reject(response);
+    })
+    .then((response) => updateNodes(response))
+    .catch((error) => error.json().then((json: any) => sendError(json.error)))
+}
+
+async function push(items: string[]) {
+  if (!credentials) {
+    return
+  }
+
+  var translations: TranslationMessage[] = []
+
+  for (const item in items) {
+    translations.push({
+      token: item,
+      translation: nodes[item]
+    })
+  }
+
+  const data = {
+    code: languageCode,
+    translations: translations
+  }
+
+  fetch(`${credentials.host}:${credentials.port}/api/plugin/push`, {
+    method: "POST",
+    headers: headers(),
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return Promise.reject(response);
+    })
+    .then((response) => console.log(JSON.stringify(response)))
+    .catch((error) => error.json().then((json: any) => sendError(json.error)))
+}
+
+async function fetchLanguages() {
+  if (!credentials) {
+    sendError("No creds")
+    return
+  }
+
+  fetch(`${credentials.host}:${credentials.port}/api/plugin/languages`, {
+    method: "GET",
+    headers: headers(),
+    redirect: 'follow',
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return Promise.reject(response);
+    })
+    .then((response) => {
+      if (response && response as string[]) {
+        languageCode = response[0]
+      }
+      send(MessageType.languages, response)
+    })
+    .catch((error) => error.json().then((json: any) => sendError(json.error)))
+}
+
+function headers(): { [name: string]: string } {
+  if (credentials) {
+    return {
+      'Access-Token': credentials.token,
+      'User-Agent': 'figma',
+      'Host': credentials.host,
+      'Content-Type': 'application/json'
+    }
+  }
+  return {}
+}
+
+// Storage
+function load() {
+  figma.clientStorage.getAsync('strings_repository_credentials')
+    .then((creds: Credentials) => {
+      if (creds) {
+        credentials = creds
+        fetchTextNodesFromSelection()
+        fetchLanguages()
+      } else {
+        send(MessageType.setup, {})
+      }
+    })
+    .catch(error => {
+      send(MessageType.setup, {})
+    })
+}
+
+function storeCreds() {
+  figma.clientStorage.setAsync('strings_repository_credentials', credentials)
+}
+
+function logout() {
+  console.log("Logged out")
+  figma.clientStorage.deleteAsync('strings_repository_credentials')
+  send(MessageType.setup, {})
+}
